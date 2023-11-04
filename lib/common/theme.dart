4ef427c99c5aa560b4a985293a06e0e7d765108e
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2023-11-05 00:06:55 +0700

    Add Theme App

---
+++ lib/common/theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Typograpghy
+FontWeight light = FontWeight.w300;
+FontWeight reguler = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+
+double defaultMargin = 24.0;
+
+// Color White, Black, Gray
+Color whiteColor = const Color(0xffFFFFFF);
+Color blackColor = Colors.black;
+Color greyColor = Colors.grey;
+
+// Jenis Font
+TextStyle fontRoboto = GoogleFonts.roboto();
+TextStyle fontPoppins = GoogleFonts.poppins();
+TextStyle fontHind = GoogleFonts.hind();
