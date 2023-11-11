99347ef43e055a34880aa8e67ce36f793a4bcf20
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2023-11-12 00:16:51 +0700

    Template File Only

---
+++ lib/view/attendance_shift.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AttendanceShift extends StatelessWidget {
+  const AttendanceShift({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Attendance Shift'),
+      ),
+    );
+  }
+}
---
+++ lib/view/attendance_square.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AttendanceSquare extends StatelessWidget {
+  const AttendanceSquare({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Attendance Square'),
+      ),
+    );
+  }
+}
